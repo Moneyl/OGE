ab6caa6c0b22e78397f7d100c314e45f1cfa2895
moneyl <8206401+Moneyl@users.noreply.github.com>
2019-11-17 21:47:24 -0500

    Initial commit

---
+++ OGE/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace OGE
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void MenuExit_OnClick(object sender, RoutedEventArgs e)
+        {
+#if DEBUG
+            var debug = 2; //Set breakpoint here for easy debugging
+#else
+            Close();
+#endif
+        }
+    }
+}
---
+++ OGE_Avalonia/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Avalonia.Interactivity;
+
+namespace OGE_Avalonia.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        public string Greeting => "Welcome to Avalonia!";
+
+        private void MenuExit_OnClick(object sender, RoutedEventArgs e)
+        {
+#if DEBUG
+            var debug = 2; //Set breakpoint here for easy debugging
+#else
+            Close();
+#endif
+        }
+    }
+}
